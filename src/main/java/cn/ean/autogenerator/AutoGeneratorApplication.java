0505418f9c5e34e93c2be2f3874e3c32035a7655
B1ackSuit <ean969648839@gmail.com>
2022-11-24 15:16:33 +0800

    Initial commit

---
+++ src/main/java/cn/ean/autogenerator/AutoGeneratorApplication.java
@@ -0,0 +1,13 @@
+package cn.ean.autogenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGeneratorApplication.class, args);
+	}
+
+}
