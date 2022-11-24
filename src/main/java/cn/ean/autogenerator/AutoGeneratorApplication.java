1503a3c08fb02a6441f4c11e16b69e2dc12a43d8
B1ackSuit <ean969648839@gmail.com>
2022-11-24 15:18:51 +0800

    simple edit pom.xml

--- src/main/java/cn/ean/autogenerator/AutoGeneratorApplication.java
+++ src/main/java/cn/ean/autogenerator/AutoGeneratorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutoGeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutoGeneratorApplication.class, args);
+    }
 
 }
